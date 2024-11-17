d049c438d42d02cbb2302fb1dd1b0e9c21931080
Abdou <nahdi.abdo.2020@gmail.com>
2024-11-17 11:24:22 +0100

    responsive added till 355px

--- index.js
+++ index.js
@@ -2,87 +2,87 @@
 // DATA :
 // IF you don't want to use local storage use THIS :
 
-// import data from "./data.json" with {type: "json"};
+import data from "./data.json" with {type: "json"};
 
 // else : localStorage IS used
 // localStorage.clear()
-localStorage.setItem(
-  "data",
-  localStorage.getItem("data") ||
-    JSON.stringify({
-      currentUser: {
-        image: {
-          png: "./images/avatars/image-juliusomo.png",
-          webp: "./images/avatars/image-juliusomo.webp",
-        },
-        username: "juliusomo",
-      },
-      comments: [
-        {
-          id: 1,
-          content:
-            "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
-          createdAt: "1 month ago",
-          score: 12,
-          user: {
-            image: {
-              png: "./images/avatars/image-amyrobson.png",
-              webp: "./images/avatars/image-amyrobson.webp",
-            },
-            username: "amyrobson",
-          },
-          replies: [],
-        },
-        {
-          id: 2,
-          content:
-            "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
-          createdAt: "2 weeks ago",
-          score: 5,
-          user: {
-            image: {
-              png: "./images/avatars/image-maxblagun.png",
-              webp: "./images/avatars/image-maxblagun.webp",
-            },
-            username: "maxblagun",
-          },
-          replies: [
-            {
-              id: 3,
-              content:
-                "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
-              createdAt: "1 week ago",
-              score: 4,
-              replyingTo: "maxblagun",
-              user: {
-                image: {
-                  png: "./images/avatars/image-ramsesmiron.png",
-                  webp: "./images/avatars/image-ramsesmiron.webp",
-                },
-                username: "ramsesmiron",
-              },
-            },
-            {
-              id: 4,
-              content:
-                "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
-              createdAt: "2 days ago",
-              score: 2,
-              replyingTo: "ramsesmiron",
-              user: {
-                image: {
-                  png: "./images/avatars/image-juliusomo.png",
-                  webp: "./images/avatars/image-juliusomo.webp",
-                },
-                username: "juliusomo",
-              },
-            },
-          ],
-        },
-      ],
-    })
-);
-let data = JSON.parse(localStorage.getItem("data"));
+// localStorage.setItem(
+//   "data",
+//   localStorage.getItem("data") ||
+//     JSON.stringify({
+//       currentUser: {
+//         image: {
+//           png: "./images/avatars/image-juliusomo.png",
+//           webp: "./images/avatars/image-juliusomo.webp",
+//         },
+//         username: "juliusomo",
+//       },
+//       comments: [
+//         {
+//           id: 1,
+//           content:
+//             "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
+//           createdAt: "1 month ago",
+//           score: 12,
+//           user: {
+//             image: {
+//               png: "./images/avatars/image-amyrobson.png",
+//               webp: "./images/avatars/image-amyrobson.webp",
+//             },
+//             username: "amyrobson",
+//           },
+//           replies: [],
+//         },
+//         {
+//           id: 2,
+//           content:
+//             "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
+//           createdAt: "2 weeks ago",
+//           score: 5,
+//           user: {
+//             image: {
+//               png: "./images/avatars/image-maxblagun.png",
+//               webp: "./images/avatars/image-maxblagun.webp",
+//             },
+//             username: "maxblagun",
+//           },
+//           replies: [
+//             {
+//               id: 3,
+//               content:
+//                 "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
+//               createdAt: "1 week ago",
+//               score: 4,
+//               replyingTo: "maxblagun",
+//               user: {
+//                 image: {
+//                   png: "./images/avatars/image-ramsesmiron.png",
+//                   webp: "./images/avatars/image-ramsesmiron.webp",
+//                 },
+//                 username: "ramsesmiron",
+//               },
+//             },
+//             {
+//               id: 4,
+//               content:
+//                 "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
+//               createdAt: "2 days ago",
+//               score: 2,
+//               replyingTo: "ramsesmiron",
+//               user: {
+//                 image: {
+//                   png: "./images/avatars/image-juliusomo.png",
+//                   webp: "./images/avatars/image-juliusomo.webp",
+//                 },
+//                 username: "juliusomo",
+//               },
+//             },
+//           ],
+//         },
+//       ],
+//     })
+// );
+// let data = JSON.parse(localStorage.getItem("data"));
 
 ///sep///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -91,7 +91,7 @@ let data = JSON.parse(localStorage.getItem("data"));
 const commentsSection = document.querySelector(".comments_container");
 const addCommentTextArea = document.querySelector(".add__comment");
 const sendBtn = document.querySelector(".send_comment-btn");
-const deleteModal = document.querySelector(".delete-modal");
+const deleteModal = document.querySelector(".delete_modal-container");
 const deleteOverlay = document.querySelector(".overlay");
 const noBtn = deleteModal.querySelector(".no_btn");
 const yesBtn = deleteModal.querySelector(".yes_btn");
